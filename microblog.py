d2f1b7f1991706d687d796babaefeafcdb55146f
Padraic Gilbert <gaelan.gilbert@gmail.com>
2022-01-05 00:05:30 -0600

    Got to chapter 5 partway

--- microblog.py
+++ microblog.py
@@ -1,5 +1,12 @@
 # finally, this is importing the entire app + routes by importing app from app
-from app import app
+from app import app, db
+from app.models import User, Post
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db,
+            'User': User,
+            'Post': Post}
 
 if __name__ == '__main__':
     app.run(debug=True)
